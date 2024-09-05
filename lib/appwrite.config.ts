d9adf0794b25a2edb8aa4146f867afa0ddd54bfa
Jim Kalake <jimkalake@gmail.com>
2024-09-05 14:21:57 +0200

    appwrite:done setting up

---
+++ lib/appwrite.config.ts
@@ -0,0 +1,12 @@
+const {PROJECT_ID,API_KEY,DO_NATION_DB,DONEE_COLLECTION_ID,DONOR_COLLECTION_ID} = process.env;
+
+import { Client, Databases, Account } from "appwrite";
+
+const client = new Client();
+client
+  .setEndpoint("https://cloud.appwrite.io/v1")
+  .setProject(PROJECT_ID!); // Replace with your project ID
+  .setKey(API_KEY!);
+
+export const account = new Account(client);
+export const databases = new Databases(client);
