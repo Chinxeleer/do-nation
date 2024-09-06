7be106ce8797991cd13b79e6a052c77bb84e7a8f
souoGael <2494554@students.wits.ac.za>
2024-09-06 06:13:55 +0200

    Insert donations feature

--- lib/appwrite.config.ts
+++ lib/appwrite.config.ts
@@ -1,15 +1,49 @@
 const {
   PROJECT_ID,
-  // API_KEY,
+  API_KEY,
   // DO_NATION_DB,
   // DONEE_COLLECTION_ID,
   // DONOR_COLLECTION_ID,
 } = process.env;
 
-import { Client, Databases, Account } from "appwrite";
+import { Client, Databases, Account } from "node-appwrite";
 
-const client = new Client();
-client.setEndpoint("https://cloud.appwrite.io/v1").setProject(PROJECT_ID!);
+type Session = { value: string } | undefined;
 
-export const account = new Account(client);
-export const databases = new Databases(client);
+const createAdminClient = async () => {
+  const client = new Client();
+  client
+    .setEndpoint("https://cloud.appwrite.io/v1")
+    .setProject(PROJECT_ID!)
+    .setKey(API_KEY!); // Replace with your project ID
+
+  return {
+    get account() {
+      return new Account(client);
+    },
+    get databases() {
+      return new Databases(client);
+    },
+  };
+};
+
+const createSessionClient = async (session: Session) => {
+  const client = new Client();
+  client.setEndpoint("https://cloud.appwrite.io/v1").setProject(PROJECT_ID!);
+  if (session) {
+    client.setSession(session.value);
+  }
+
+  return {
+    get account() {
+      return new Account(client);
+    },
+    get databases() {
+      return new Databases(client);
+    },
+  };
+};
+export { createAdminClient, createSessionClient };
+// export const account = new Account(client);
+// export const databases = new Databases(client);
+//
